854366e72c3ec04352ccd0790acd785052c319bc
yuanj <yuanjing@365yzg.com>
2021-04-19 11:05:58 +0800

    [add]2021.04.19 (整合ruoyi作为admin后台管理系统)

---
+++ admin/src/main/java/com/feichai/admin/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package com.feichai.admin.quartz.util;
+
+import com.feichai.admin.quartz.domain.SysJob;
+import org.quartz.JobExecutionContext;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author ruoyi
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
